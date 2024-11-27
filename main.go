a692eaf477c046bc9d1bf7370fae345e7ce6d7a1
moatas19m <sayyid.moatasim2@gmail.com>
2024-11-27 21:05:23 +0500

    feat: This is showcasing the difference between http.Handle and http.HandleFunc

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+//func main() {
+//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+//		w.Write([]byte("Hello World"))
+//	})
+//	http.ListenAndServe(":8080", nil)
+//}
+
+type ExampleHandler struct{}
+
+func (h *ExampleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World (whispered from inside the ExampleHandler) :D This is http.Handle()")
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
-	http.ListenAndServe(":8080", nil)
+	myHandler := &ExampleHandler{}
+	http.Handle("/", myHandler)
+	err := http.ListenAndServe(":8080", &ExampleHandler{})
+	if err != nil {
+		return
+	}
 }
