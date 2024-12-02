a7d4fe7b4ca5b2d6d0bc313105f429864f7a4c73
moatas19m <sayyid.moatasim2@gmail.com>
2024-12-02 15:12:56 +0500

    chore: Custom and simple handler functions together for clarity and understanding

--- main.go
+++ main.go
@@ -5,24 +5,31 @@ import (
 	"net/http"
 )
 
-//func main() {
-//	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-//		w.Write([]byte("Hello World"))
-//	})
-//	http.ListenAndServe(":8080", nil)
-//}
-
+// ExampleHandler demonstrates a custom handler
 type ExampleHandler struct{}
 
+// ServeHTTP implements the http.Handler interface
 func (h *ExampleHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World (whispered from inside the ExampleHandler) :D This is http.Handle()")
 }
 
+// Simpler handler to be used by HandleFunc
+func simpleHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World, this is http.HandleFunc()")
+}
+
 func main() {
-	myHandler := &ExampleHandler{}
-	http.Handle("/", myHandler)
-	err := http.ListenAndServe(":8080", &ExampleHandler{})
+	// Custom handler using http.Handle
+	myCustomHandler := &ExampleHandler{}
+	http.Handle("/custom", myCustomHandler)
+
+	// Simple handler using http.HandleFunc
+	http.HandleFunc("/simple", simpleHandler)
+
+	// Start the server
+	err := http.ListenAndServe(":9000", nil)
 	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
 		return
 	}
 }
