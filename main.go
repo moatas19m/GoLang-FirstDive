7bef176789a2043ed74ed0c1b71be6196b91087a
moatas19m <sayyid.moatasim2@gmail.com>
2024-12-04 17:12:43 +0500

    feat(Pipelines): Very basic pipeline

--- main.go
+++ main.go
@@ -1,64 +1,31 @@
 package main
 
 import (
-	"GoLang-FirstDive/viewmodel"
 	"html/template"
-	"io/ioutil"
-	"net/http"
 	"os"
 )
 
-func main() {
-	templates := populateTemplates()
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		requestedFile := r.URL.Path[1:]
-		template := templates[requestedFile+".html"]
-		context := viewmodel.NewBase()
-		if template != nil {
-			template.Execute(w, context)
-		} else {
-			w.WriteHeader(http.StatusNotFound)
-		}
-	})
-	http.Handle("/css/", http.FileServer(http.Dir("./css/")))
-	http.ListenAndServe(":9000", nil)
+const tax = 6.75 / 100
+
+type Product struct {
+	Name  string
+	Price float64
 }
 
-func populateTemplates() map[string]*template.Template {
-	result := make(map[string]*template.Template)
-	const basePath = "templates"
-	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
-	template.Must(layout.ParseFiles(basePath+"/header.html", basePath+"/footer.html"))
+func (p Product) PriceWithTax() float64 {
+	return p.Price * (1 + tax)
+}
 
-	dir, err := os.Open(basePath + "/content")
-	if err != nil {
-		panic("Cannot open template directory: " + err.Error())
-	}
+const templateString = `
+{{- "Item Info:" }}
+Name: {{ .Name }}
+Price: {{ printf "$%.2f" .Price}}
+Price With Tax: {{ .PriceWithTax | printf "%.2f" }}
+`
 
-	files, err := dir.Readdir(-1)
-	if err != nil {
-		panic("Cannot read template directory: " + err.Error())
-	}
+func main() {
+	p := Product{"Pizza", 10}
 
-	for _, file := range files {
-		f, err := os.Open(basePath + "/content/" + file.Name())
-		if err != nil {
-			panic("Cannot open template file: " + file.Name() + err.Error())
-		}
-
-		content, err := ioutil.ReadAll(f)
-		if err != nil {
-			panic("Cannot read template file: " + file.Name() + err.Error())
-		}
-
-		f.Close()
-
-		tmpl := template.Must(layout.Clone())
-		_, err = tmpl.Parse(string(content))
-		if err != nil {
-			panic("Cannot parse template: " + file.Name() + err.Error())
-		}
-		result[file.Name()] = tmpl
-	}
-	return result
+	t := template.Must(template.New("templateString").Parse(templateString))
+	t.Execute(os.Stdout, p)
 }
